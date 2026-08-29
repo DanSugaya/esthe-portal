fbc5bfe58ce28242a4e316bfae2a8339ecc9fd01
Dan <shiningroad.0211@gmail.com>
2026-08-29 22:21:43 +0900

    ga4設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 // app/layout.tsx
 import Header from '@/components/Header'
-import AuthBar from '@/components/AuthBar' // ログインバーを追加する場合
+import AuthBar from '@/components/AuthBar'
 import Footer from '@/components/Footer'
 import './globals.css'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function RootLayout({
   children,
@@ -12,10 +13,20 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className="bg-gray-100 text-gray-800 font-sans antialiased">
+        {/* ↓ bodyの直下に noscript タグを配置 */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M5244CG5"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <div className="w-full max-w-md mx-auto min-h-screen bg-white flex flex-col justify-between">
           <div>
             <Header />
-            <AuthBar /> {/* ヘッダーの直下にログイン・新規登録バーを配置 */}
+            <AuthBar />
           </div>
 
           <div className="flex-1">
@@ -25,6 +36,7 @@ export default function RootLayout({
           <Footer />
         </div>
       </body>
+      <GoogleTagManager gtmId="GTM-M5244CG5" />
     </html>
   )
 }
